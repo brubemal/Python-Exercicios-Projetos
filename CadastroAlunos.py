7b76ec41054b2aa0fb0d6bae046e7ad3b224567f
Bruno Bertolini Maluf <brubemal@gmail.com>
2026-03-12 16:42:43 -0300

    adiciona funcao remover da tabela cadastra_aluno

--- CadastroAlunos.py
+++ CadastroAlunos.py
@@ -10,11 +10,11 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS cadastro_alunos(
                )""")
 
 
-
 while True:
     print("\n[1]Cadastrar Aluno")
     print("[2]Listar Alunos")
-    print("[3]Cancelar")
+    print("[3]Remover Aluno")
+    print("[4]Cancelar")
 
     opcao = input("\nEscolha uma das opcoes: ")
 
@@ -22,18 +22,23 @@ while True:
         nome = input("Nome do aluno a ser cadastrado: ")
         ra = input("Digite seu RA: ")
         cursor.execute("INSERT INTO cadastro_alunos (nome, ra) VALUES (?,?)",
-        (nome, ra)
-        )
+                       (nome, ra)
+                       )
 
         conexao.commit()
 
     elif opcao == '2':
         cursor.execute("SELECT * FROM cadastro_alunos")
 
-        alunos = cursor.fetchall() 
+        alunos = cursor.fetchall()
 
         for aluno in alunos:
             print(aluno)
 
     elif opcao == '3':
+        nome = (input("Digite o nome da pessoa que deseja remover da tabela: "))
+        cursor.execute("DELETE FROM cadastro_alunos WHERE nome = ?", (nome,))
+        conexao.commit()
+
+    elif opcao == '4':
         break
