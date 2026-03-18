c9c9e1b7deefc7f88e2de7525385d4762571e663
brubemal <brubemal@gmail.com>
2026-03-18 12:32:06 -0300

    Alterando maneira de ordenar o vetor, agora usando variavel auxiliar

--- OrdenarVet.py
+++ OrdenarVet.py
@@ -4,8 +4,10 @@ for i in vet:
     vet[i] = int(input(f"Digite o {i+1}o numero"))
 
 for i in range(len(vet)):
-    for j in range(len(vet) - 1 - i):
-        if vet[j] > vet[j + 1]:
-            vet[j], vet[j + 1] = vet[j + 1], vet[j]
+    for j in range(i + 1, len(vet)):
+        if vet[i] > vet[j]:
+            aux = vet[i]
+            vet[i] = vet[j]
+            vet[j] = aux
 
 print(vet)
